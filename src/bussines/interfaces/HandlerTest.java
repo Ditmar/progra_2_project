990fc524abdc542632a471bae89625114787c169
Ditmar <spyatorio@gmail.com>
2025-06-30 17:04:11 -0400

    feat: se agrego el examen

---
+++ src/bussines/interfaces/HandlerTest.java
@@ -0,0 +1,7 @@
+package bussines.interfaces;
+
+import bussines.model.Credential;
+
+public interface HandlerTest {
+    void clickLogin(Credential credential);
+}
---
+++ src/services/ServerTest.java
@@ -0,0 +1,8 @@
+package services;
+
+public class ServerTest {
+    public ServerTest() {
+        super();
+        System.out.println("HOLA MUNDO");
+    }
+}
