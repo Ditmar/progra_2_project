69753dbd7d61afc17005da218dd4e78f797879b8
Lander Delgado Cruz <clander395@gmail.com>
2025-06-30 15:35:22 -0400

    mi segundo parcial

--- src/bussines/LoginManagement.java
+++ src/bussines/LoginManagement.java
@@ -5,35 +5,39 @@ import javax.swing.SwingUtilities;
 
 import bussines.model.Credential;
 import services.LoginServices;
+import services.UserService;
 import ui.DashBoard;
 import ui.LoginWindow;
 
 public class LoginManagement {
     private LoginWindow mainWindow;
     private LoginServices loginServices;
+    private UserService userService;
 
     public LoginManagement() {
         initServices();
         SwingUtilities.invokeLater(() -> {
-            mainWindow = new LoginWindow("Hola Mundo");
+            mainWindow = new LoginWindow("Lander piuin Login");
             mainWindow.setOnLoginCallBack((credential) -> {
                 validateFlowCredentials(credential);
             });
         });
+    }
+    private void initServices() {
+        loginServices = new LoginServices();
+        userService = new UserService();
     }
 
     private void validateFlowCredentials(Credential credential) {
         Boolean isValid = loginServices.isValidLogin(credential.getUserName(), credential.getPassword());
         if (isValid) {
             mainWindow.setVisible(false);
-            new DashBoard("DashBoard");
+            new DashBoard("DashBoard", mainWindow, userService);
         } else {
             JOptionPane.showMessageDialog(mainWindow, "Las credenciasles no son correctas", "Login Error",
                     JOptionPane.ERROR_MESSAGE);
         }
     }
 
-    private void initServices() {
-        loginServices = new LoginServices();
-    }
+    
 }
